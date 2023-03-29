4e5030c18f14300fb45f99f6bb073d662551473b
saketbyte <samriddhsingh00@gmail.com>
2023-03-29 23:35:56 +0530

    React component and folder structure and props

---
+++ Section_3_React_Components/01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+// The file name should be each subword starting with capital.
+// A component in react is just a JS /JSX function.
+
+function ExpenseItem(){
+
+    return(
+        <div>
+            <div>March 29th 2023</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>₹300</div>
+            </div>
+        </div>
+    )
+}
+
+// exporting it as a default.
+export default ExpenseItem;
