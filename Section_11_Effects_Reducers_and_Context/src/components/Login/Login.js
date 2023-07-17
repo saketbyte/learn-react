dd6bf7c76bfdac0b847bba6a043600938adbd20f
saketbyte <samriddhsingh00@gmail.com>
2023-07-17 13:00:15 +0530

    struggled with context and sideEffect explanation in course, but youtube was so much better

--- Section_11_Effects_Reducers_and_Context/src/components/Login/Login.js
+++ Section_11_Effects_Reducers_and_Context/src/components/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+// You must add all "things" you use in your effect function if those "things" could change because your component (or some parent component) re-rendered. That's why variables or state defined in component functions, props or functions defined in component functions have to be added as dependencies!
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
@@ -24,12 +24,15 @@ const Login = (props) => {
       console.log("Checking form validity!");
       setFormIsValid(enteredEmail.includes("@") && enteredPassword.trim().length > 6);
     }, 500);
-
+    // We want the above function to run for each keystroke entered and not just once on the first render.
+    // We add a timer here so that if any HTTP requests are to be sent they are sent after some time. here 500ms. [Debouncing]
+    // Any action in response to some other action is where useEffect hook should be used.
     return () => {
       console.log("CLEANUP");
       clearTimeout(identifier);
     };
-  }, [enteredEmail, enteredPassword]);
+  }, [enteredEmail, enteredPassword]); // enteredEmail and enteredPassword are our two dependencies for this.
+  // We can omit the function setFormIsValid because it is itself a state handler, and will never change, ensured by React backend.
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
--- Section_11_contextAPI/src/App.js
+++ Section_11_contextAPI/src/App.js
@@ -3,40 +3,43 @@ import React, { useState, useEffect } from "react";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    // To access the data which was stored in cache with the name isLoggedIn.
     const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
 
     if (storedUserLoggedInInformation === "1") {
       setIsLoggedIn(true);
     }
-  }, []); // Runs only when the dependencies change.
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    // to store something in cache for the time being.
-    localStorage.setItem("isLoggedIn", "1"); // a token and an identifier string.
-    setIsLoggedIn(true); // state update to re-render
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
     localStorage.removeItem("isLoggedIn");
-    setIsLoggedIn(false); // runs when log out changes the state variable.
+    setIsLoggedIn(false);
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler, // We can add this onlogout in the initial definition of context with an empty function as value.
+      }}>
+      <MainHeader />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
